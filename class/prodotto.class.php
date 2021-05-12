fe9f66cb70f2aad70be64c701c97bd1b6a255ba9
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-05-12 16:23:46 +0200

    add. class videogiochi

---
+++ class/prodotto.class.php
@@ -0,0 +1,35 @@
+<?php 
+
+class Videogiochi {
+
+    // attributi
+
+    private $nome;
+    private $piattaforma;
+    private $prezzo;
+    private $sconto;
+    private $spedizione;
+
+    // cotruttore
+
+    public function __construct ($nome, $piattaforma, $prezzo, $sconto, $spedizione){
+
+        $this -> nome =
+        $nome;
+        $this->piattaforma =
+        $piattaforma;
+        $this->prezzo =
+        $prezzo;
+        $this->sconto =
+        $sconto;
+        $this->spedizione =
+        $spedizione;
+
+    }
+
+    // metodi
+}
+
+
+
+?>
